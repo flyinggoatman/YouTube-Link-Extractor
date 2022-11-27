2ab8f9a5965d72bf61779b2019176c321721c354
flyinggoatman <flyinggoatman@outlook.com>
2022-11-27 16:14:13 +0000

    added discord support

--- Link Extreactor.py
+++ Link Extreactor.py
@@ -11,7 +11,9 @@ import base64
 import os
 import webbrowser
 import pyperclip
-
+import win32com.client as comclt
+import time
+import pyautogui
 
 
 
@@ -21,27 +23,29 @@ if search("http", channel):
     if re.search("://", channel):
         if re.search("youtu", channel):
             
+            # Loads page data #
             soup = BeautifulSoup(requests.get(channel, cookies={'CONSENT': 'YES+1'}).text, "html.parser")
-            
             data = re.search(r"var ytInitialData = ({.*});", str(soup.prettify())).group(1)
-            
             json_data = json.loads(data)
             
+            # Finds channel information #
             channel_id   = json_data["header"]["c4TabbedHeaderRenderer"]["channelId"]
             channel_name = json_data["header"]["c4TabbedHeaderRenderer"]["title"]
             channel_logo = json_data["header"]["c4TabbedHeaderRenderer"]["avatar"]["thumbnails"][2]["url"]
             channel_id_link = "https://youtube.com/channel/"+channel_id
             
+            
+            # Prints Channel information to console #
             print("Channel ID: "+channel_id)
             print("Channel Name: "+channel_name)
             print("Channel Logo: "+channel_logo)
             print("Channel ID: "+channel_id_link)
             
-
+            # Creates HTML file var# 
             f = open('channelid.html','w')
             
         
-
+            # Converts and downlaods image file to png # 
             imgUrl = channel_logo
             filename = "image.png".split('/')[-1]
             r = requests.get(imgUrl, allow_redirects=True)
@@ -49,7 +53,7 @@ if search("http", channel):
             
             
             
-
+            # Creates HTML file #
             message = """<html>
             <head></head>c
             <body>
@@ -64,11 +68,28 @@ if search("http", channel):
 
             f.write(message)
             f.close()
-
-            webbrowser.open_new_tab('channelid.html')
+            # Opens HTNL file in new browser. #
+            webbrowser.open_new('channelid.html')
+            
+            # Copies channel ID url to clipboard. #
             pyperclip.copy(channel_id_link)
-else:
+            
+            #Opens discord #
+            discord = '#discord channel link#'
+            webbrowser.open_new_tab(discord)
+            time.sleep(8)
+            with pyautogui.hotkey('ctrl', 'v'):
+                 pyautogui.press('Enter')
+            
+            
+            
+            # pyautogui.hold('ctrl'):
+            #     pyautogui.press(['v'])
+            #         pyautogui.hotkey()
+#             The effect is that calling hotkey('ctrl', 'shift', 'c') would perform a "Ctrl-Shift-C" hotkey/keyboard shortcut press.
+# else:
     easygui.msgbox("Llnk not supported.")
+    
                         
                         
                         # https://www.youtube.com/@elespiritudelvolcan/channels
